add90d11001767568488939ada0d17e88217f9b5
Omrani Mootez Belleh <114734614+Refereeing@users.noreply.github.com>
2023-01-05 03:09:21 +0100

    Add files via upload

--- src/Components/MovieList/MovieList.jsx
+++ src/Components/MovieList/MovieList.jsx
@@ -1,1 +1,29 @@
+import React from 'react';
+import { useState } from 'react';
+import Filter from '../Filter/Filter';
+import MovieCard from '../MovieCard/MovieCard';
 
+const MovieList = ({films}) => {
+
+    const [text, setText] = useState('')
+    const [rate, setRate] = useState('')
+    
+    const filtertext = (text) => {
+        setText(text)
+    }
+    const filterrate = (rate) => {
+        setRate(rate)
+    }
+    return (
+            <div>
+                <Filter filtertext={filtertext} filterrate={filterrate} />
+                <div style={{ display: 'flex', justifyContent: 'space-around', flexWrap: "wrap" }} className="adc">
+                    {films.filter((el) => 
+                    (el.rate.toString() >= rate && el.title.toLowerCase().includes(text.toLowerCase()) )).map((element) =>
+                        <MovieCard movie={element}/>
+                )}
+                </div>
+            </div>
+    )
+};
+export default MovieList
